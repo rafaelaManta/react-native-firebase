5f21d62b32ea6e714c0bc7c53b25d463b3acc431
rafaela-1 <rafaela@novoville.com>
2023-10-13 15:08:45 +0300

    wip

--- App.js
+++ App.js
@@ -22,33 +22,57 @@ import { useFcm, fcmOnMessage, } from './services/FcmNotifications';
 
 const App = () => {
   const fcmToken = useFcm()
+  //const {allowedVersions} = useBootraping()
+  //const isUserLoggedIn = useIsUserLoggedIn()
+  //const isUserUnauth = useIsEmailOfUnauth()
+  //isEmailOfUnauth(store.getState()?.profileReducer.profile.email)
+  //const didTheAppHasInitialScreen= useInitialModules()
+  //store.getState()?.otherReducer?.authorityDetails?.initial_module
+  const isUserLoggedIn = true
+
 
 
   useEffect(() => {
-    const unsubscribeFcmOnMessage = fcmOnMessage()
+    //check if the app needs update
+    //else setError and return
 
-    //background event
-    messaging().onNotificationOpenedApp(remoteMessage => {
-      console.log(
-        'Notification caused app to open from background state:',
-        remoteMessage.notification,
-      );
+    //set up the app language
 
-    });
+    if (isUserLoggedIn) {
+      const unsubscribeFcmOnMessage = fcmOnMessage()
+      //getBadgeNumber
+      //setBadgeNumber to the redux store
 
-    //quite event
-    messaging()
-      .getInitialNotification()
-      .then(remoteMessage => {
-        if (remoteMessage) {
-          console.log(
-            'Notification caused app to open from quit state:',
-            remoteMessage.notification,
-          );
-        }
+      //background event
+      messaging().onNotificationOpenedApp(remoteMessage => {
+        console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage.notification,
+        );
+        //navigate to screen
       });
 
-    return unsubscribeFcmOnMessage;
+      //quite event
+      messaging()
+        .getInitialNotification()
+        .then(remoteMessage => {
+          if (remoteMessage) {
+            console.log(
+              'Notification caused app to open from quit state:',
+              remoteMessage.notification,
+            );
+            //navigate to screen
+          }
+        });
+
+      //run the code for the module initial screen from api
+
+
+      return unsubscribeFcmOnMessage
+
+    } else {
+      //run the code for bbbApp
+    }
 
   }, [])
 
